53b1f8df0b7e183eea2ca2d43bba1219a4023b00
Elian Doran <contact@eliandoran.me>
2022-01-09 01:52:58 +0200

    Add scripts to build Less & copy to clipboard

---
+++ build-nixos.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i sh -p lessc xclip
+sh ./build.sh
---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+script_dir=$(dirname $(realpath $0))
+output_path="$script_dir/output.css"
+
+lessc $script_dir/src/master.less > $output_path && \
+    echo Saved to "$output_path" && \
+    cat $output_path | xclip -selection c && \
+    echo Copied to clipboard.
