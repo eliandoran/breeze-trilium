7c0baecf997509e2561fcbe19419f689fc61624d
Elian Doran <contact@eliandoran.me>
2022-01-09 02:28:55 +0200

    Rename output file to "theme.css"

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 script_dir=$(dirname $(realpath $0))
-output_path="$script_dir/output.css"
+output_path="$script_dir/theme.css"
 
 lessc $script_dir/src/master.less > $output_path && \
     echo Saved to "$output_path" && \
