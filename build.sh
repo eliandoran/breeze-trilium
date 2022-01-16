12fc4f84b3e994d114bff7b3d9f2860089432c3e
Elian Doran <contact@eliandoran.me>
2022-01-16 12:17:28 +0200

    Build both light and dark themes

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 script_dir=$(dirname $(realpath $0))
-output_path="$script_dir/theme.css"
+output_dir="$script_dir"
 
-lessc $script_dir/src/master.less > $output_path && \
-    echo Saved to "$output_path" && \
-    cat $output_path | xclip -selection c && \
-    echo Copied to clipboard.
+lessc $script_dir/src/breeze-light.less > "$output_dir/breeze-light.css"
+lessc $script_dir/src/breeze-dark.less > "$output_dir/breeze-dark.css"
+
