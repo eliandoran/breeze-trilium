c66f486f86f7ab7a5f28bb14c43d08b1a87b3717
Elian Doran <contact@eliandoran.me>
2022-01-16 12:25:10 +0200

    Reimplement copy to clipboard for both themes

--- build.sh
+++ build.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
 script_dir=$(dirname $(realpath $0))
 output_dir="$script_dir"
 
-lessc $script_dir/src/breeze-light.less > "$output_dir/breeze-light.css"
-lessc $script_dir/src/breeze-dark.less > "$output_dir/breeze-dark.css"
+function build_theme() {
+    theme=$1
+    output_path="$output_dir/breeze-$theme.css"
+    echo "Building theme "$theme": $output_path"
+    lessc "$script_dir/src/breeze-$theme.less" > "$output_path"
+}
 
+if [ $# -eq 1 ]; then
+    build_theme $1 && \
+        cat "$output_path" | xclip -selection c && \
+        echo "Copied to clipboard."
+    exit 0
+fi
+
+build_theme "light"
+build_theme "dark"
