14b2262cc077984c9ad66d893603814dbf1b0d92
Elian Doran <contact@eliandoran.me>
2023-11-14 20:13:29 +0200

    Add CI workflow

--- build-nixos.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -i bash -p lessc xclip
-./build.sh "$@"
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-script_dir=$(dirname "$(realpath $0)")
-output_dir="$script_dir"
-
-output_path="$output_dir/breeze.css"
-echo "Building theme: $output_path"
-lessc "$script_dir/src/color-scheme/breeze-auto.less" > "$output_path"
