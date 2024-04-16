613f7cf62d57f457aeaea26c12ddb956e0d20b97
Elian Doran <contact@eliandoran.me>
2024-04-16 19:30:13 +0300

    build: Fix upload script

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 source ./upload.local.sh
-./build.sh && curl http://localhost:37840/etapi/notes/$NOTE_ID/content \
+lessc "src/color-scheme/breeze-auto.less" > breeze.css && curl http://localhost:37840/etapi/notes/$NOTE_ID/content \
   --header "Authorization: $ETAPI_TOKEN" \
   --header "Content-Type: text/plain" \
   --upload-file breeze.css
